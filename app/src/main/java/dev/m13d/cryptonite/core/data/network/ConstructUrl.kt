06c943e6c0d6765b5931489d45863f6e7155c1c4
Michael <3yebmb@gmail.com>
2024-10-31 19:51:49 +0100

    Add "constructor" the URL

---
+++ app/src/main/java/dev/m13d/cryptonite/core/data/network/ConstructUrl.kt
@@ -0,0 +1,11 @@
+package dev.m13d.cryptonite.core.data.network
+
+import dev.m13d.cryptonite.BuildConfig
+
+fun constructUrl(url: String): String {
+    return when {
+        url.contains(BuildConfig.BASE_URL) -> url
+        url.startsWith("/") -> BuildConfig.BASE_URL + url.drop(1)
+        else -> BuildConfig.BASE_URL + url
+    }
+}
